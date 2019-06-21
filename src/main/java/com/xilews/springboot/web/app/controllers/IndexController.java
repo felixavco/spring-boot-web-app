2099ef34c505a25412fc244d9a157fccf9b93259
Felix Avelar <felixavco@gmail.com>
2019-06-21 10:39:41 -0600

    added bootstrap

--- src/main/java/com/xilews/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/xilews/springboot/web/app/controllers/IndexController.java
@@ -1,5 +1,8 @@
 package com.xilews.springboot.web.app.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.xilews.springboot.web.app.models.User;
 
 @Controller
-@RequestMapping("/app")
+@RequestMapping({"/user", "/app", "/users"})
 public class IndexController {
 
 	@GetMapping({ "/", "/home", "/index", "" })
@@ -24,10 +27,24 @@ public class IndexController {
 		User user = new User();
 		user.setFirstName("Felix");
 		user.setLastName("Avelar");
+		user.setEmail("felix@gmail.com");
+		
 		
 		model.addAttribute("title", "Profile " + user.getFirstName());
 		model.addAttribute("user", user);
 		
 		return "user/profile";
+	}
+	
+	@GetMapping("/list")
+	public String list(Model model) {
+		List<User> users = new ArrayList<>();
+		users.add(new User("Felix", "Avelar", "felix@gmail.com"));
+		users.add(new User("Camila", "Avelar", "camila@gmail.com"));
+		users.add(new User("Brenda", "Marroquin", "brenda@gmail.com"));
+		
+		model.addAttribute("title", "List of users");
+		model.addAttribute("users", users);
+		return "user/list";
 	}
 }
