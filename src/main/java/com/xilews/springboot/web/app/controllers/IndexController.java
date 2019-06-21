4e0e557b6f067f67900631ff62f85137cb3630ca
Felix Avelar <felixavco@gmail.com>
2019-06-21 09:11:32 -0600

    2nd commit

--- src/main/java/com/xilews/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/xilews/springboot/web/app/controllers/IndexController.java
@@ -3,15 +3,31 @@ package com.xilews.springboot.web.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.xilews.springboot.web.app.models.User;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
-	@GetMapping({ "/", "/home", "/index" })
+	@GetMapping({ "/", "/home", "/index", "" })
 	public String index(Model model) {
 		
 		model.addAttribute("title", "Spring Framework");
 		
 		return "index";
+	}
+	
+	@GetMapping("/profile")
+	public String profile(Model model) {
+		User user = new User();
+		user.setFirstName("Felix");
+		user.setLastName("Avelar");
+		
+		model.addAttribute("title", "Profile " + user.getFirstName());
+		model.addAttribute("user", user);
+		
+		return "user/profile";
 	}
 }
---
+++ src/main/java/com/xilews/springboot/web/app/models/User.java
@@ -0,0 +1,47 @@
+package com.xilews.springboot.web.app.models;
+
+public class User {
+	
+	private String firstName; 
+	private String lastName; 
+	private String email;
+
+	//* Constructors 
+	public User() {
+		
+	}
+	
+	public User(String firstName, String lastName, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+	
+	
+	//* Getters & Setters
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	} 	
+	
+}
